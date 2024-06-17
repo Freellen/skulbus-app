6496a6f36895999e2dfc6df2480b00aa8ac154ab
frdrckaman <frdrckaman@gmail.com>
2024-06-17 16:33:14 +0300

    update template

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_map_app/pages/welcome.dart';
 
 import 'pages/google_map_page.dart';
+import 'pages/login.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,6 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         title: 'Skulbus App',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: const GoogleMapPage(),
+        // home: const GoogleMapPage(),
+        // home: const Login(),
+        home: const Welcome(),
       );
 }
